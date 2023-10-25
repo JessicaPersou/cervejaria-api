76a7b88003b20bc0c95591a4f0599db7c76cf918
Jessica Sousa <jessica.persou@gmail.com>
2023-10-24 22:37:17 -0300

    documentatio swagger

--- src/main/java/br/com/jps/cervejariaapi/CervejariaApiApplication.java
+++ src/main/java/br/com/jps/cervejariaapi/CervejariaApiApplication.java
@@ -2,6 +2,7 @@ package br.com.jps.cervejariaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class CervejariaApiApplication {
--- src/main/java/br/com/jps/cervejariaapi/controller/ClientController.java
+++ src/main/java/br/com/jps/cervejariaapi/controller/ClientController.java
@@ -4,6 +4,9 @@ import br.com.jps.cervejariaapi.business.AddressBusiness;
 import br.com.jps.cervejariaapi.business.ClientBusiness;
 import br.com.jps.cervejariaapi.dto.AddressDTO;
 import br.com.jps.cervejariaapi.dto.ClientDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
@@ -15,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/client")
+@Api(description = "Endpoints to management of Clients ")
 public class ClientController {
 
     @Autowired
@@ -24,37 +28,42 @@ public class ClientController {
     private AddressBusiness addressBusiness;
 
     @GetMapping
+    @ApiOperation(httpMethod = "GET", value = "List all Clients", notes = "List all Clients", response = ClientDTO.class)
     public ResponseEntity<List<ClientDTO>> allClients(){
         return ResponseEntity.ok(clientBusiness.findAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ClientDTO> ClientById(@PathVariable Long id){
+    public ResponseEntity<ClientDTO> ClientById(@ApiParam("client id") @PathVariable Long id){
         return ResponseEntity.ok(clientBusiness.clientById(id));
     }
 
     @GetMapping("/document")
-    public ResponseEntity<List<ClientDTO>> allClientsByDocument(@Param("document")String document){
+    @ApiOperation(httpMethod = "GET", value = "List Clients by Document", notes = "List Clients by Document", response = ClientDTO.class)
+    public ResponseEntity<List<ClientDTO>> allClientsByDocument(@ApiParam("Document Number") @Param("document")String document){
         return ResponseEntity.ok(clientBusiness.clientsByDocument(document));
     }
 
+    @ApiOperation(httpMethod = "GET", value = "List Clients by Profile", notes = "List Clients by Profile", response = ClientDTO.class)
     @GetMapping("/profile")
     public ResponseEntity<List<ClientDTO>> clientsProfile(){
         return ResponseEntity.ok(clientBusiness.clientStatusProfileDisabled());
     }
 
     @PostMapping
+    @ApiOperation(httpMethod = "POST", value = "Created New Client", notes = "Created New Client", response = ClientDTO.class)
     public ResponseEntity<ClientDTO> newClient(@Valid @RequestBody ClientDTO clientDTO){
         ClientDTO newClient = clientBusiness.createNewClient(clientDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(newClient);
     }
 
+    @ApiOperation(httpMethod = "PUT", value = "Update Client", notes = "Update Client", response = ClientDTO.class)
     @PutMapping("/{id}")
     public ResponseEntity<ClientDTO> clientUpdate(@PathVariable Long id, @Valid @RequestBody ClientDTO clientDTO){
         return ResponseEntity.ok(clientBusiness.clientUpdate(id,clientDTO));
     }
 
-    //desativando perfil do client
+    @ApiOperation(httpMethod = "POST", value = "Disable Client", notes = "Disable Client", response = ClientDTO.class)
     @PostMapping ("disabled/{id}")
     public ResponseEntity<ClientDTO> disableClient(@PathVariable Long id){
         return ResponseEntity.ok(clientBusiness.disableClient(id));
@@ -62,23 +71,27 @@ public class ClientController {
 
     /*** Address-Controller ***/
 
+    @ApiOperation(httpMethod = "GET", value = "List All Addresses", notes = "List All Addresses", response = AddressDTO.class)
     @GetMapping("{id}/address")
     public ResponseEntity<List<AddressDTO>> AddressByClient(@PathVariable Long id){
         return ResponseEntity.ok(addressBusiness.findAddressByClientId(id));
     }
 
+    @ApiOperation(httpMethod = "POST", value = "Create New Address", notes = "Create New Address", response = AddressDTO.class)
     @PostMapping("{clientId}/address/")
     public ResponseEntity<AddressDTO> newAddress(@PathVariable Long clientId, @RequestBody AddressDTO addressDTO){
         return ResponseEntity.status(HttpStatus.CREATED).body(addressBusiness.createNewAddress(clientId,addressDTO));
     }
 
+    @ApiOperation(httpMethod = "PUT", value = "Update Address", notes = "Update Address", response = AddressDTO.class)
     @PutMapping("{idClient}/address/{idAddress}")
-    public ResponseEntity<AddressDTO> updateAddress(@PathVariable Long idClient,@PathVariable Long idAddress, @RequestBody AddressDTO addressDTO){
+    public ResponseEntity<AddressDTO> updateAddress(@PathVariable Long idClient,@PathVariable Long idAddress,@RequestBody AddressDTO addressDTO){
         return ResponseEntity.status(HttpStatus.OK).body(addressBusiness.updateAddress(idClient, idAddress, addressDTO));
     }
 
+    @ApiOperation(httpMethod = "DELETE", value = "Delete Address", notes = "Delete Address", response = AddressDTO.class)
     @DeleteMapping("{idClient}/address/{idAddress}")
-    public ResponseEntity deleteAddress(@PathVariable Long idClient,@PathVariable Long idAddress){
+    public ResponseEntity deleteAddress(@PathVariable Long idClient, @PathVariable Long idAddress){
         addressBusiness.deleteAddress(idClient,idAddress);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Endereço excluído com sucesso.");
     }
