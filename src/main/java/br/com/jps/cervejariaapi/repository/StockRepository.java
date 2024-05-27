47687895dc8e5bea3e4e134b32f9b8e4af8ace29
Jessica Sousa <jessica.sousa@koin.com.br>
2024-05-26 22:43:28 -0300

    initial commit

--- src/main/java/br/com/jps/cervejariaapi/repository/StockRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.jps.cervejariaapi.repository;
-
-import br.com.jps.cervejariaapi.model.Stock;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StockRepository extends JpaRepository<Stock, Long> {
-}
